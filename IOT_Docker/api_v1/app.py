15a2b5bfc562ea9d05ef8d7f3c7d1c14884db6a7
Alexandre <alexandreaanp@gmail.com>
2023-05-22 15:58:05 +0100

    fix volumes errors

--- IOT_Docker/api_v1/app.py
+++ IOT_Docker/api_v1/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 #from flask_crontab import Crontab
 import requests
 import threading
@@ -9,13 +9,13 @@ from mqttController import MQTTClient
 
 ALLOWED_EXTENSIONS = ['mp4']
 VIDEOS_PATH = "../videos"
-ListenerMQTT = MQTTClient.getMQTTClient(hostname="localhost", port=2222, subscribeList=["raspberrypi1", "raspberrypi2"])
+ListenerMQTT = MQTTClient.getMQTTClient(hostname="iot_docker-mosquitto-mqtt-1", port=1883, subscribeList=["raspberrypi1", "raspberrypi2"])
 t1 = threading.Thread(target=ListenerMQTT.getClient().loop_start)
 
 
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = '../videos'
+app.config['UPLOAD_FOLDER'] = VIDEOS_PATH
 
 
 #function to terminated when ctrl + c is pressed
@@ -31,19 +31,23 @@ def hello_world():
     print(MQTTClient.getMQTTClient().isSubscribe("/teste/"))
     return 'Hello World'
 
-
 @app.route('/setVideo', methods=["GET"])
 def setRaspBerryPiVideo():
-     videoName=request.args.get("videoName",None)
+     videoName = request.args.get("videoName",None)
      topic = request.args.get("topic",None)
      #if there are parameters variables videoName and topic
      if(videoName and topic):
-        print(os.listdir(VIDEOS_PATH))
         #if there is not a '.' and if the filename is not in the videos folder
-        if(not '.'in videoName and not videoName in os.listdir(VIDEOS_PATH)):
+        if('.'in videoName and not videoName in os.listdir(VIDEOS_PATH)):
             return f'videoName not found in videos folder {videoName}', 404
-        
+        if(not topic in MQTTClient.getMQTTClient().getSubscribedList()):
+            return f'API MQTT Client is not subscribe to '+topic, 404
+        return f'OK'
      return "missing url parameters, you should use 'videoName' and 'topic parameters'",500
+
+@app.route('/getSubscribedTopic', methods=["GET"])
+def getSubscribedTopic():
+    return jsonify(MQTTClient.getMQTTClient().getSubscribedList())
 
 @app.route('/uploadvideo', methods=["POST"])
 def uploadVideo():

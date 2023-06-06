b7ca621bded6200d4f9d99c9d529a251f1906dbc
Alexandre <alexandreaanp@gmail.com>
2023-06-06 22:24:04 +0100

    Added bootstrap, new page and failed page

--- WebClient/webClient/app/views.py
+++ WebClient/webClient/app/views.py
@@ -1,13 +1,46 @@
 from django.shortcuts import render
 import requests
 import json
-
+API_HOST = "localhost"
+API_PORT = "5000"
 def minha_pagina(request):
-    opcoes_raspberry = json.loads(requests.get("http://localhost:5000/getSubscribedTopic").content.decode())
-    videos = json.loads(requests.get("http://localhost:5000/getVideo").content.decode())
+    msgErr = ""
+    try:
+        opcoes_raspberry = json.loads(requests.get("http://"+API_HOST+":"+API_PORT+"/getSubscribedTopic").content.decode())
+        videos = json.loads(requests.get("http://"+API_HOST+":"+API_PORT+"/getVideo").content.decode())
+    except Exception as e:
+        msgErr = e.__str__
+        return render(request, 'FailedConnect.html', {
+            "API_HOST": API_HOST,
+            "API_PORT" : API_PORT,
+            "msgErr": msgErr
+            })
+            
     context = {
         'opcoes_raspberry': opcoes_raspberry,
         'opcoes_videos' : videos
     }
 
     return render(request, 'minha_pagina.html', context)
+
+def new_minha_pagina(request):
+    msgErr = ""
+    try:
+        opcoes_raspberry = json.loads(requests.get("http://"+API_HOST+":"+API_PORT+"/getSubscribedTopic").content.decode())
+        videos = json.loads(requests.get("http://"+API_HOST+":"+API_PORT+"/getVideo").content.decode())
+    except Exception as e:
+        msgErr = e.__str__
+        return render(request, 'FailedConnect.html', {
+            "API_HOST": API_HOST,
+            "API_PORT" : API_PORT,
+            "msgErr": msgErr
+            })
+            
+    context = {
+        "API_HOST": API_HOST,
+        "API_PORT" : API_PORT,
+        'opcoes_raspberry': opcoes_raspberry,
+        'opcoes_videos' : videos
+    }
+
+    return render(request, 'new_minha_pagina.html', context)
